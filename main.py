730c968cc8faac9e7a7c7b62771903faccda9852
AsafViana <67490242+AsafViana@users.noreply.github.com>
2022-02-17 13:18:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+#dir = os.path.dirname(os.path.realpath(__file__))
+
+
+a = open('arquivos nomes.txt', 'wt+')
+
+dir = input('> ')
+with open('arquivos nomes.txt', 'a') as txt:
+
+  for diretorio, subpastas, arquivos in os.walk(dir):
+    if 'venv' not in diretorio:
+      for arquivo in arquivos:
+        if '/' in diretorio:
+          livros = diretorio + '\\' + arquivo
+          txt.write(livros+'\n')
+          print(livros)
